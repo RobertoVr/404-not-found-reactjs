b3891b2ac592047e925705873610747b174afbad
Roberto Valdez <robert.tito94@gmail.com>
2020-10-05 16:31:53 -0500

    Updates

---
+++ precache-manifest.a60d3ae0ff7202ca49edf6e81195d1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b17fec1e4b9021297936afeb3700bae",
+    "url": "/404-not-found-reactjs/index.html"
+  },
+  {
+    "revision": "4526d83045137e019aec",
+    "url": "/404-not-found-reactjs/static/css/main.fa4108bc.chunk.css"
+  },
+  {
+    "revision": "e8f466889061e3cd76f9",
+    "url": "/404-not-found-reactjs/static/js/2.2592a01a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404-not-found-reactjs/static/js/2.2592a01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4526d83045137e019aec",
+    "url": "/404-not-found-reactjs/static/js/main.38c26737.chunk.js"
+  },
+  {
+    "revision": "3dde53b82b525665b87d",
+    "url": "/404-not-found-reactjs/static/js/runtime-main.b7393269.js"
+  }
+]);
